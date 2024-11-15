e061ada0842417a6a79bbaebbd295a59d4a25240
Geonu Yu <dbrjdn2051@naver.com[C>
2024-11-15 20:11:20 +0900

    refactor (SearchControllerTest) : 피드백 반영

--- src/test/java/org/example/expert/domain/search/Controller/SearchControllerTest.java
+++ src/test/java/org/example/expert/domain/search/Controller/SearchControllerTest.java
@@ -17,7 +17,10 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.List;
-import static org.mockito.ArgumentMatchers.*;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
